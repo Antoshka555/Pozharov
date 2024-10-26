44c03cb203fc81aaf6fb70c700979a640f16199a
Antoshka555 <pozharov2005men@gmail.com>
2024-10-26 11:34:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -21,3 +21,53 @@ $query = $db->select(true,"*","anton1","task_id='1'");
 
 print_r($query);
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Мой задачник</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="tasks">
+	<div class="task">
+		<div class="task-control">
+			<input type="checkbox" name="">
+		</div>
+		<div class="task-text">Прийти на пару</div>
+		<div class="task-date">
+			<input type="date" name="">
+		</div>
+	</div>
+	
+	<div class="task">
+		<div class="task-control">
+			<input type="checkbox" name="">
+		</div>
+		<div class="task-text">Прийти на пару</div>
+		<div class="task-date">
+			<input type="date" name="">
+		</div>
+	</div>
+	<div class="task">
+		<div class="task-control">
+			<input type="checkbox" name="">
+		</div>
+		<div class="task-text">Прийти на пару</div>
+		<div class="task-date">
+			<input type="date" name="">
+		</div>
+	</div>
+	<div class="task">
+		<div class="task-control">
+			<input type="checkbox" name="">
+		</div>
+		<div class="task-text">Прийти на пару</div>
+		<div class="task-date">
+			<input type="date" name="">
+		</div>
+	</div>
+</div>
+</body>
+</html>
